ab00d0d4f007daf8d240f860f5b18619b68c836e
hailhan <hailhan@uchicago.edu>
2026-05-11 09:57:25 -0500

    State and ZCTA functionality

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -5,27 +5,31 @@ library(ggplot2)
 # takes a geographic boundary (either list of ZCTAs or states) as input
 # uses Tigris to return a basemap of those geographic boundaries for the given year
 get_basemap <- function(zcta_list, state, year){
-  # default to earliest year in tigris
-  if (year < 2000) {
-    year = 2000
-  }
 
   if (missing(zcta_list)) {
     # default to latest year for state boundaries
-    if (year > 2010){
+    if (year > 2010) {
       year = 2010
+    } else if (year < 2000) {
+      year = 2000
+    } else {
+      year = year
     }
-    boundary <- zctas(state = state, year = year, cb = TRUE)
+    boundary <- zctas(state = state, year = year, cb = FALSE)
   }
+
   if (missing(state)) {
+    # default to earliest year in tigris
+    if (year < 2000) {
+      year = 2000
+    }
     boundary <- zctas(starts_with = zcta_list, year = year, cb = TRUE)
   }
-  return
-    ggplot(data = boundary) +
-      geom_sf() +
-      theme_void()
 
+  ggplot(data = boundary) +
+      geom_sf(fill = NA, color = "black") +
+      theme_void()
 }
 
-CA <- get_basemap(state="IL", year = 2020)
+CA <- get_basemap(state = "IL", year = 2005)
 CA
