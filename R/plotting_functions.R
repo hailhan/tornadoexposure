b4ae0dcafce3a9479c05f5ad85f49f35c9fdf2db
hailhan <hailhan@uchicago.edu>
2026-05-11 10:38:11 -0500

    Updating documentation for base functions

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1,25 +1,43 @@
-library(tigris)
-library(sf)
-library(ggplot2)
-
-# takes a geographic boundary (list of ZCTAs) as input
-# uses Tigris to return a basemap of those geographic boundaries for the given year
-get_basemap <- function(zcta_list, year){
-
+#' Retrieve ZCTA geometries from Tigris
+#'
+#' Takes a list of US ZCTA codes and a year, and returns a simple features
+#' geometry object for those ZCTA boundaries in the given year
+#'
+#' @param zcta_list Vector of ZCTAs (or ZCTA prefixes)
+#' @note ZCTAs/prefixes can be passed in as characters or integers
+#' @note ZCTAs/prefixes can be 1-5 characters
+#' @param year Census year for requested geometries
+#'
+#' @return An sf object containing ZCTA boundary geometries
+#'
+#' @keywords internal
+get_geometry <- function(zcta_list, year){
   # default to closest year in tigris
   valid_years <- c(2000, 2010, 2020)
   year <- max(valid_years[valid_years <= year])
 
-  boundary <- zctas(
+  boundary <- tigris::zctas(
     starts_with = as.character(zcta_list),
     year = year,
     cb = TRUE)
-
-  ggplot(data = boundary) +
-      geom_sf(fill = NA, color = "black") +
-      theme_void()
 }
 
-# works with any length of ZCTA provided (eg. 6, 603, 60304)
-IL <- get_basemap(zcta_list = c(60304), year = 2005)
-IL
+#' Generate basemap for ZCTA boundaries
+#'
+#' Takes a list of US ZCTA codes and a year, and returns a plot of the requested
+#' ZCTA boundaries
+#'
+#' @param zcta_list Vector of ZCTAs (or ZCTA prefixes)
+#' @note ZCTAs/prefixes can be passed in as characters or integers
+#' @note ZCTAs/prefixes can be 1-5 characters
+#' @param year Census year for requested geometries
+#'
+#' @return An plot object mapping boundaries for requested ZCTAs
+#'
+#' @keywords internal
+get_basemap <- function(zcta_list, year){
+  boundary_geom <- get_geometry(zcta_list, year)
+  ggplot2::ggplot(data = boundary_geom) +
+    geom_sf(fill = NA, color = "black") +
+    theme_void()
+}
